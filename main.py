e536f1c0c30d6c4a6a1103f0eddc8760b235e4a9
Денис <mr.vaynbaum@mail.ru>
2022-09-28 20:22:09 +0300

    изменение фронта

--- main.py
+++ main.py
@@ -8,34 +8,42 @@ from neural_network.neural_network import *
 
 FORM = tk.Tk()
 FORM.title(TITLE)
-FORM.geometry("570x420")
+FORM.geometry("570x350")
 FORM.config(bg="#CFDBDA")
 FORM.resizable(width=0, height=0)
 
 neural_net = NeuralNetwork()
-filename=None
-fontt = Font(family='Segoe UI Symbol', size=16)
-def create_out_path(filename:str):
-    words = filename.split('.')
-    string = "".join(words[:len(words)-1])
-    return f'{string}_output.avi'
+filename = None
+fontt = Font(family="Segoe UI Symbol", size=16)
 
-def cut_filename(filename:str):
-    res = filename.split('/')
-    return "/".join(res[len(res)-2:])
 
-def link(*args):
-    neural_net.OpenVideo('assets/demo.mp4')
+def create_out_path(filename: str):
+    words = filename.split(".")
+    string = "".join(words[: len(words) - 1])
+    return f"{string}_output.avi"
+
+
+def cut_filename(filename: str):
+    res = filename.split("/")
+    return "/".join(res[len(res) - 2 :])
+
+
 def link2(*args):
     global filename
     filename = askopenfilename(filetypes=filetypes)
     if filename != "":
-        button3['state'] = tk.NORMAL
-        button4['state'] = tk.NORMAL
+        button3["state"] = tk.NORMAL
+        button4["state"] = tk.NORMAL
         labelopfile.config(text=cut_filename(filename))
+        label.config(text="")
+        FORM.update()
+
+
 def link3(*args):
     if filename is not None:
         neural_net.OpenVideo(filename)
+
+
 def link4(*args):
     label.place(x=195, y=265)
     label.config(text="Video Processing...")
@@ -45,22 +53,9 @@ def link4(*args):
     label.place(x=135, y=265)
     label.config(text="Processing completed successfully")
     FORM.update()
-    sleep(2)
-    label.config(text="")
-    FORM.update()
-button = tk.Button(
-    FORM,
-    command=link,
-    padx=175,
-    pady=3,
-    text="Open demo video",
-    bd=0,
-    bg="#8AAAA5",
-    underline=0,
-    cursor="hand2",
-    font=fontt
-)  # Инициализация кнопки
-#button.grid(column=0, row=3, pady=4, padx=25)
+
+
+# button.grid(column=0, row=3, pady=4, padx=25)
 button2 = tk.Button(
     FORM,
     command=link2,
@@ -71,9 +66,9 @@ button2 = tk.Button(
     bg="#8AAAA5",
     underline=0,
     cursor="hand2",
-    font=fontt
+    font=fontt,
 )  # Инициализация кнопки
-#button2.grid(column=0, row=0, pady=4, padx=25)
+# button2.grid(column=0, row=0, pady=4, padx=25)
 button3 = tk.Button(
     FORM,
     command=link3,
@@ -85,9 +80,9 @@ button3 = tk.Button(
     underline=0,
     cursor="hand2",
     state=tk.DISABLED,
-    font=fontt
+    font=fontt,
 )  # Инициализация кнопки
-#button3.grid(column=0, row=2, pady=4, padx=25)
+# button3.grid(column=0, row=2, pady=4, padx=25)
 button4 = tk.Button(
     FORM,
     command=link4,
@@ -99,9 +94,9 @@ button4 = tk.Button(
     underline=0,
     cursor="hand2",
     state=tk.DISABLED,
-    font=fontt
+    font=fontt,
 )  # Инициализация кнопки
-#button4.grid(column=0, row=1, pady=4, padx=25)
+# button4.grid(column=0, row=1, pady=4, padx=25)
 label = tk.Label(text="", bg="#CFDBDA", fg="#708479")
 label.place(x=195, y=265)
 labelopfile = tk.Label(text="", bg="#CFDBDA", fg="#708479")
@@ -110,18 +105,14 @@ labelmainname = tk.Label(text="Detect medical mask on people", bg="#CFDBDA")
 
 label.config(font=fontt)
 labelopfile.config(font=fontt)
-labelmainname.config(font=('Segoe UI Semibold', 20))
+labelmainname.config(font=("Segoe UI Semibold", 20))
 
 labelmainname.pack(pady=15, anchor=W, padx=25)
 button2.pack(pady=5, anchor=W, padx=25)
 button3.pack(pady=5, anchor=W, padx=25)
 button4.pack(pady=5, anchor=W, padx=25)
-button.pack(pady=50, anchor=W, padx=25)
 
-filetypes = [
-            ("Video File1", "*.mp4"),
-            ("Video File2", "*.avi")
-            ]
+filetypes = [("Video File1", "*.mp4"), ("Video File2", "*.avi")]
 
 
 def focus_inb(event):
@@ -134,8 +125,6 @@ def focus_outb(event):
     event.widget.configure(fg="#000")
 
 
-button.bind("<Enter>", focus_inb)
-button.bind("<Leave>", focus_outb)
 button2.bind("<Enter>", focus_inb)
 button2.bind("<Leave>", focus_outb)
 button3.bind("<Enter>", focus_inb)
